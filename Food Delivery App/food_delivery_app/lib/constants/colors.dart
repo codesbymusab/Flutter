7212e5261248a3b5268f44fd234c7408fb854541
codesbymusab <154744844+codesbymusab@users.noreply.github.com>
2026-01-05 12:28:31 +0500

    Delete Food Delivery App/food_delivery_app directory

--- Food Delivery App/food_delivery_app/lib/constants/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-final scaffoldColor = Color.fromRGBO(255, 201, 0, 1);
-final contrastColor = Color.fromRGBO(164, 99, 0, 1);
-final backgroundColor = const Color.fromARGB(255, 248, 231, 156);
