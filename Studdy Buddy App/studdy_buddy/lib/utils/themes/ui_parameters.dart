8b79b7032a5eebf4af59ac391af31f2229eb6209
codesbymusab <154744844+codesbymusab@users.noreply.github.com>
2026-01-05 12:28:56 +0500

    Delete Studdy Buddy App/studdy_buddy directory

--- Studdy Buddy App/studdy_buddy/lib/utils/themes/ui_parameters.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class UIParameters {
-  static bool isDarkMode(BuildContext context) {
-    return MediaQuery.of(context).platformBrightness == Brightness.dark;
-  }
-}
